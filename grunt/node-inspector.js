4540bac0b186fa3725af9da66b62b60c3ff7c832
Rogelio Morrell <molekilla@gmail.com>
2015-03-10 18:48:59 -0500

    updated node inspector config

--- grunt/node-inspector.js
+++ grunt/node-inspector.js
@@ -3,7 +3,9 @@ module.exports = {
   dev: {
     options: {
       'web-port': '<%= nodeInspector.webPort %>',
-      'debug-port': '<%= nodeInspector.debugPort %>'
+      'debug-port': '<%= nodeInspector.debugPort %>',
+      'save-live-edit': '<%= nodeInspector.saveLiveEdit || false %>',
+      'hidden': ['node_modules']
     }
   }
 };
