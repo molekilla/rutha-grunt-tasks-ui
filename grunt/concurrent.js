d858dfcbb1c383183d545bbca02791639f8be582
Rogelio Morrell <molekilla@gmail.com>
2015-05-25 07:46:20 -0700

    Update concurrent.js

--- grunt/concurrent.js
+++ grunt/concurrent.js
@@ -2,19 +2,22 @@ module.exports = {
   test: {
     tasks: ['nodemon', 'protractor:run'],
     options: {
-      logConcurrentOutput: true
+      logConcurrentOutput: true,
+      limit: 2
     }
   },
   auto: {
     tasks: ['nodemon', 'node-inspector', 'browserSync', 'watch'],
     options: {
-      logConcurrentOutput: true
+      logConcurrentOutput: true,
+      limit: 4
     }
   },
   dev: {
     tasks: ['nodemon', 'node-inspector', 'watch'],
     options: {
-      logConcurrentOutput: true
+      logConcurrentOutput: true,
+      limit: 3
     }
   }
 };
